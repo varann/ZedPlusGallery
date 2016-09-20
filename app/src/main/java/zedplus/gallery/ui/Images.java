d6885a0118432d0f367c282a06eb1f36baf10488
Anna Savinova <varannn@gmail.com>
2016-09-20 17:31:06 +0300

    Image withrounded corners

--- app/src/main/java/zedplus/gallery/ui/Images.java
+++ app/src/main/java/zedplus/gallery/ui/Images.java
@@ -1,8 +1,15 @@
 package zedplus.gallery.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 /**
  * @author Anna Savinova
@@ -14,9 +21,48 @@ public class Images {
 	public static void load(ImageView view, String url) {
 		Picasso.with(view.getContext())
 				.load(url)
+				.transform(new RoundedTransformation(16, 0))
 				.fit()
-				.centerInside()
 				.into(view);
+	}
+
+	private static class RoundedTransformation implements Transformation {
+
+		private int radius;
+		private int margin;
+
+		public RoundedTransformation(int radius, int margin) {
+			this.radius = radius;
+			this.margin = margin;
+		}
+
+		@Override
+		public Bitmap transform(Bitmap source) {
+			final Paint paint = new Paint();
+			paint.setAntiAlias(true);
+			paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP,
+					Shader.TileMode.CLAMP));
+
+			Bitmap output = Bitmap.createBitmap(source.getWidth(),
+					source.getHeight(), Bitmap.Config.ARGB_8888);
+			Canvas canvas = new Canvas(output);
+			canvas.drawRoundRect(
+					new RectF(margin, margin, source.getWidth() - margin, source.getHeight() - margin),
+					radius,
+					radius,
+					paint);
+
+			if (source != output) {
+				source.recycle();
+			}
+
+			return output;
+		}
+
+		@Override
+		public String key() {
+			return "rounded";
+		}
 	}
 
 }
